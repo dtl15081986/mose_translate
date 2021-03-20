984aec71fce982b305f03bb735fe215ecd5f24cc
Dominique Torres <dtl15081986@gmail.com>
2021-03-20 16:39:32 -0300

    morse

--- codigomorse.py
+++ codigomorse.py
@@ -4,11 +4,12 @@ data = pandas.read_csv("C:/Users/domin/OneDrive/Desktop/Python/100 days of code/
 df = pandas.DataFrame(data)
 dictionary = df.to_dict()
 
+
 morse_dict= {dictionary["letter"][n]: dictionary["code"][n] for n in dictionary["letter"]}
 
 word = input("What is you word?\n").upper()
 
 pal_list = [morse_dict[n] for n in list(word) if n in morse_dict]     
 
-print(f'{word} = {(pal_list)}')
+print(f'{word} = {(pal_list)} aa')
 
